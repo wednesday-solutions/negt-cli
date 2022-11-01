ac44995a4ec0854ab6c49c34ca1a52b8bb4e2d64
Ijas Mohamad <ijas@wednesday.is>
2022-11-01 20:18:01 +0530

    fix: bugs fixed

--- utils/testUtils/subjectsMockData.js
+++
@@ -1,9 +0,0 @@
-import range from 'lodash/range';
-import faker from 'faker';
-
-export const subjectsTable = range(1, 10).map((_, index) => ({
-	id: (index + 1).toString(),
-	name: faker.name.firstName(),
-	class: 10,
-	chapter: 10
-}));
